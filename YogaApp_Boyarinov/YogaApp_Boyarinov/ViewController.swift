0866863cc8ad4b83fcd8a8483ba834f73af7a1b1
PiksarLive <76645171+PiksarLive@users.noreply.github.com>
2022-06-06 22:28:53 +0300

    Add files via upload

---
+++ YogaApp_Boyarinov/YogaApp_Boyarinov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YogaApp_Boyarinov
+//
+//  Created by Student on 18.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
